b9d2498a24fdaea939c48667e77873694aee998b
Ratimon <ratixoxo@gmail.com>
2023-03-30 13:48:32 +0700

    Build(./src & ./script): Adding Overmint2 challenge#re-entrancy

--- Makefile
+++ Makefile
@@ -5,11 +5,14 @@ anvil-node:
 anvil-node-auto:
 	anvil --chain-id 1337 --block-time 5
 
-2-deploy-overmint1:
+1-deploy-overmint1:
 	forge script DeployOvermint1Script --rpc-url $(call local_network,8545)  -vvvv --broadcast; \
 
 1-unit:
 	forge test --match-path test/Overmint1.t.sol -vvv
+
+2-deploy-overmint2:
+	forge script DeployOvermint2Script --rpc-url $(call local_network,8545)  -vvvv --broadcast; \
 
 define local_network
 http://127.0.0.1:$1
---
+++ script/2_DeployOvermint2.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.19;
+
+import {Script} from "@forge-std/Script.sol";
+import {Overmint2} from "@main/Overmint2.sol";
+
+contract DeployOvermint2Script is Script {
+    Overmint2 overmint2sChallenge;
+
+    function run() public {
+        // uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        // string memory mnemonic = vm.envString("MNEMONIC");
+
+        // address is already funded with ETH
+        string memory mnemonic ="test test test test test test test test test test test junk";
+        uint256 deployerPrivateKey = vm.deriveKey(mnemonic, "m/44'/60'/0'/0/", 1); //  address = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        overmint2sChallenge = new Overmint2();
+
+        vm.stopBroadcast();
+    }
+}
