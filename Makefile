e77292d8176b0b7ef5a9ccc200f3627afedd965c
Ratimon <ratixoxo@gmail.com>
2023-04-27 17:44:04 +0700

    Build(./src & ./script): Adding Read Only challenge#12

--- Makefile
+++ Makefile
@@ -71,6 +71,9 @@ anvil-node-auto:
 11-unit:
 	forge test --match-path test/11_Flashloan.t.sol -vvv
 
+12-deploy-readonly:
+	forge script DeployReadOnlyScript --rpc-url $(call local_network,8545)  -vvvv --broadcast; \
+
 define local_network
 http://127.0.0.1:$1
 endef
---
+++ script/12_DeployReadOnly.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.19;
+
+import {Script} from "@forge-std/Script.sol";
+import {ReadOnlyPool, VulnerableDeFiContract} from "@main/ReadOnly.sol";
+
+contract DeployReadOnlyScript is Script {
+    ReadOnlyPool readOnlyPool;
+    VulnerableDeFiContract vulnerableDeFiContract;
+
+    function run() public {
+        // uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        // string memory mnemonic = vm.envString("MNEMONIC");
+
+        // address is already funded with ETH
+        string memory mnemonic ="test test test test test test test test test test test junk";
+        uint256 deployerPrivateKey = vm.deriveKey(mnemonic, "m/44'/60'/0'/0/", 1); //  address = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        readOnlyPool = new ReadOnlyPool();
+        vulnerableDeFiContract = new VulnerableDeFiContract(readOnlyPool);
+
+        readOnlyPool.addLiquidity{value: 100 ether}();
+        readOnlyPool.earnProfit{value: 1 ether}();
+        vulnerableDeFiContract.snapshotPrice();
+
+        vm.stopBroadcast();
+    }
+}
